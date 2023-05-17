65a887181899da6dd82357786a1a8db6028b9be6
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2023-05-17 15:33:14 +0200

    fix(auth.middleware.ts): fix header error by adding return statement

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
   if (!req.session.authenticated) {
-    res.status(403).send({ message: 'Not authenticated' });
+    return res.status(403).send({ message: 'Not authenticated' });
   }
   next();
 };
