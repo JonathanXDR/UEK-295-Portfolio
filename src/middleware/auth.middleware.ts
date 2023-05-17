4b70efe9e9eca834b9fb6ca99934c466e57d3e9c
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2023-05-17 13:49:16 +0200

    chore(cleanup auth.middleware.ts): remove else statement

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
-  if (req.session.authenticated) {
-    next();
-  } else {
+  if (!req.session.authenticated) {
     res.status(403).send({ message: 'Not authenticated' });
   }
+  next();
 };
