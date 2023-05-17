4d3de3ed004f480d0cb1eec2304e43ed36388c5e
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2023-05-17 13:47:55 +0200

    chore(cleanup auth.ts): remove else statements

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -9,32 +9,31 @@ declare module 'express-session' {
 }
 
 router.post('/login', (req: Request, res: Response) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  const { email, password } = req.body;
 
-  if (email && password === 'm295') {
-    req.session.authenticated = true;
-    req.session.email = email;
-    res.status(200).send({ message: 'Authenticated successfully' });
-  } else {
-    res.status(401).send({ message: 'Invalid credentials' });
+  if (!email || password !== 'm295') {
+    return res.status(401).send({ message: 'Invalid credentials' });
   }
+
+  req.session.authenticated = true;
+  req.session.email = email;
+  return res.status(200).send({ message: 'Authenticated successfully' });
 });
 
 router.get('/verify', (req: Request, res: Response) => {
-  if (req.session.authenticated) {
-    res
-      .status(200)
-      .send({ message: 'Token is valid', email: req.session.email });
-  } else {
-    res.status(401).send({ message: 'Token is invalid' });
+  if (!req.session.authenticated) {
+    return res.status(401).send({ message: 'Token is invalid' });
   }
+
+  return res
+    .status(200)
+    .send({ message: 'Token is valid', email: req.session.email });
 });
 
 router.delete('/logout', (req: Request, res: Response) => {
   req.session.authenticated = false;
   req.session.email = null;
-  res.sendStatus(204);
+  return res.sendStatus(204);
 });
 
 export default router;
